35a19c82e5a3fe2a5906d347e134ba7a96fa8866
Rafael Alves <rafaealves013@gmail.com>
2025-07-18 16:54:07 -0300

    jornal

--- scripts.js
+++ scripts.js
@@ -9,16 +9,16 @@ toggleButton.addEventListener('click', () => {
 // CONTEÚDO DAS NOTÍCIAS
 const noticias = {
   noticia1: {
-    titulo: "Tremores assustam moradores da Savassi",
-    corpo: "Moradores relatam tremores inexplicáveis durante três noites consecutivas. Especialistas não encontraram falhas geológicas. Testemunhas afirmam ter ouvido sussurros vindo do chão."
+    titulo: "Bolsonaro de Tornozeleira",
+    corpo: "BRASÍLIA - O ex-presidente Jair Bolsonaro (PL) declarou que se sentiu “humilhado” com a operação da Polícia Federal (PL) nesta sexta-feira (18). Agentes estiveram na casa dele e na sede do PL, em Brasília, para cumprir mandados de busca e apreensão.O objetivo é uma suprema humilhação”, disse. “Sou ex-presidente, tenho 70 anos de idade, isso é uma suprema humilhação”, repetiu. Ele falou ao sair da Secretaria de Administração Penitenciária do Distrito Federal, onde colocou tornozeleira eletrônica.Agora monitorado, Bolsonaro afirmou que “nunca” pensou em sair do Brasil ou ir para uma embaixada. Ele também se disse inocente no processo de golpe de Estado, em que é réu no Supremo Tribunal Federal (STF). “Nada me coloca em um plano golpista que não existe”, frisou. Na operação desta sexta-feira, a PF apreendeu, na casa do ex-presidente, R$ 7 mil e US$ 14 mil. Bolsonaro afirmou que o dinheiro não é ilícito e pode ter origem comprovada. “Sempre guardei dólar em casa. É normal”, frisou. Ele também ressaltou não saber sobre um pen drive encontrado pelos agentes no banheiro de sua casa.  "
   },
   noticia2: {
     titulo: "Galeria subterrânea é interditada após desabamento",
     corpo: "A galeria na região central foi interditada após estranhos colapsos internos. Símbolos antigos foram encontrados nas paredes. A área segue isolada pelas autoridades."
   },
   noticia3: {
-    titulo: "Depoimento de Bolsonaro ao STF",
-    corpo: "Denúncia da PGR apontou ex-presidente Jair Bolsonaro como líder de organização criminosa que planejou suposta tentativa de golpe de Estado em 2022"
+    titulo: "Nas selvas de Belize, arqueólogos desenterraram o que pode ser a tumba de um rei maia",
+    corpo: "Nas selvas de Belize, arqueólogos desenterraram o que pode ser a tumba de um rei maia com 1.700 anos de idade — possivelmente o governante fundador da antiga cidade de Caracol. Encontrada sob um palácio próximo à imponente estrutura Caana, a tumba continha um verdadeiro tesouro de artefatos da elite: uma máscara mortuária de jade e conchas, contas de jade com gravuras de macaco-aranha, cerâmicas cerimoniais e três pares de brincos de jade — extremamente raros em sepultamentos maias. O tamanho e o conteúdo da tumba sugerem fortemente um status real, e sua data estimada, por volta de 350 d.C., coincide com o reinado de Te K’ab Chaak, o primeiro rei conhecido de Caracol.Embora a tumba não contenha inscrições, os arqueólogos Diane e Arlen Chase, que trabalham em Caracol há décadas, estão 99,9% confiantes de que ela pertenceu a Te K’ab Chaak. Se confirmada, seria o sepultamento real mais antigo encontrado no local e pode alterar nossa compreensão das relações entre os maias e Teotihuacan, sugerindo contato décadas antes do que é amplamente aceito. Ainda assim, alguns especialistas pedem cautela até que surjam evidências por DNA ou inscrições. A equipe planeja compartilhar mais informações em uma conferência futura e espera que testes adicionais confirmem a identidade do ocupante da tumba e esclareçam aspectos da política mesoamericana antiga. Fonte: University of Houston. (10 de julho de 2025). UH Archae"
   },
   noticia4: {
     titulo: "Atlético Mineiro",
